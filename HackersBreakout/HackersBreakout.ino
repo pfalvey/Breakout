a09e0b8a7fc92caa1cf8fc1c13f3fd718ea47dc0
Alex Ayala <aayala4@gitlab.com>
2019-02-06 16:04:51 -0500

    Update HackersBreakout.ino

--- HackersBreakout/HackersBreakout.ino
+++ HackersBreakout/HackersBreakout.ino
@@ -8,7 +8,7 @@ BeepPin1 beep;
 boolean playing = false;
 const unsigned int rows = 7;
 const unsigned int columns = 8;
-int columnOffset = 128 - columns*5;
+int columnOffset = 128 - columns * 5;
 boolean bricks[rows][columns]; //false for not hit
 const unsigned int paddleSize = 10;
 const unsigned int paddleX = 10;
@@ -37,25 +37,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (lives == 0){
+  if (lives == 0) {
     arduboy.clear();
     arduboy.print("Game over");
   }
-  if (hitCount == columns * rows){
+  if (hitCount >= columns * rows) {
     arduboy.clear();
     arduboy.print("You win!");
   }
-  while(!playing) {
-    if(arduboy.pressed(A_BUTTON)) {
+  while (!playing) {
+    if (arduboy.pressed(A_BUTTON)) {
       arduboy.clear();
       initialDrawBlocks();
       playing = true;
-    }   
+    }
   }
   if (!arduboy.nextFrame()) {
-      return;
-  }  
-  
+    return;
+  }
+
   movePaddle();
   moveBall();
   arduboy.display();
@@ -63,9 +63,9 @@ void loop() {
 
 void movePaddle() {
   arduboy.drawRect(paddleX, paddleY, 1, paddleSize, 0);
-  if(arduboy.pressed(UP_BUTTON) && paddleY > 0) {
+  if (arduboy.pressed(UP_BUTTON) && paddleY > 0) {
     paddleY -= 1;
-  } else if(arduboy.pressed(DOWN_BUTTON) && paddleY < 54) {
+  } else if (arduboy.pressed(DOWN_BUTTON) && paddleY < 54) {
     paddleY += 1;
   }
   arduboy.drawRect(paddleX, paddleY, 1, paddleSize, 1);
@@ -77,11 +77,11 @@ void moveBall() {
     arduboy.drawRect(extraBallX, extraBallY, 2, 2, 0);
   }
   //If the player presses the launch button give the ball velocity
-  if(arduboy.pressed(B_BUTTON) && !ballLaunched) {
+  if (arduboy.pressed(B_BUTTON) && !ballLaunched) {
     dX = 1;
-    dY = random(0,2)*2-1;
+    dY = random(0, 2) * 2 - 1;
     ballLaunched = true;
-  } else if(!ballLaunched) {
+  } else if (!ballLaunched) {
     //otherwise have it move with the paddle
     ballX = paddleX + 1;
     ballY = paddleY + 4;
@@ -99,61 +99,61 @@ void moveBall() {
 }
 
 void bounceWall() { //(duplicate all conditionals for extraBall, if it is in play)
-  if(ballY < 0) {
+  if (ballY < 0) {
     ballY = 0;
     dY = -dY;
   }
-  if(ballY > 62) {
+  if (ballY > 62) {
     ballY = 62;
     dY = -dY;
   }
-  if(ballX > 126) {
+  if (ballX > 126) {
     ballX = 126;
     dX = -dX;
   }
   //Now do same wall checking for extra ball
-  if(extraBall) {
-    if(extraBallY < 0) {
+  if (extraBall) {
+    if (extraBallY < 0) {
       extraBallY = 0;
       extraDY = -extraDY;
     }
-    if(extraBallY > 62) {
+    if (extraBallY > 62) {
       extraBallY = 62;
       extraDY = -extraDY;
     }
-    if(extraBallX > 126) {
+    if (extraBallX > 126) {
       extraBallX = 126;
       extraDX = -extraDX;
     }
   }
   //bounce off paddle
-  if (ballX <= 11 && ballX > 9 && dX < 0){
-    if (ballY >= paddleY && ballY <= paddleY + paddleSize){
+  if (ballX <= 11 && ballX > 9 && dX < 0) {
+    if (ballY >= paddleY && ballY <= paddleY + paddleSize) {
       dX = -dX;
-      dY = ((ballY-(paddleY+5))/3);
-      if (dY == 0){
-        dY = random(0,2)*2-1;
+      dY = ((ballY - (paddleY + 5)) / 3);
+      if (dY == 0) {
+        dY = random(0, 2) * 2 - 1;
       }
     }
   }
   //bounce extraBall off paddle
-  if(extraBall) {
-    if (extraBallX <= 11 && extraBallX > 9 && extraDX < 0){
-      if (extraBallY >= paddleY && extraBallY <= paddleY + paddleSize){
+  if (extraBall) {
+    if (extraBallX <= 11 && extraBallX > 9 && extraDX < 0) {
+      if (extraBallY >= paddleY && extraBallY <= paddleY + paddleSize) {
         extraDX = -extraDX;
-        extraDY = ((extraBallY-(paddleY+5))/3);
-        if (extraDY == 0){
-          extraDY = random(0,2)*2-1;
+        extraDY = ((extraBallY - (paddleY + 5)) / 3);
+        if (extraDY == 0) {
+          extraDY = random(0, 2) * 2 - 1;
         }
       }
     }
   }
   //lose life when bottom hit
   //if extra ball is in play, no penalty, just lose extra ball
-  if(extraBall) {
-    if(ballX < -1) {
+  if (extraBall) {
+    if (ballX < -1) {
       //ballX = paddleX+1;
-      //ballY = paddleY + 4; 
+      //ballY = paddleY + 4;
       //dX = 0;
       //dY = 0;
       //ballLaunched = false;
@@ -169,7 +169,7 @@ void bounceWall() { //(duplicate all conditionals for extraBall, if it is in pla
       extraDY = 0;
       extraBall = false;
     }
-    if(extraBallX < -1) {
+    if (extraBallX < -1) {
       //if the extraBall dies, just make it disappear
       extraBallX = 0;
       extraBallY = 0;
@@ -180,9 +180,9 @@ void bounceWall() { //(duplicate all conditionals for extraBall, if it is in pla
   }
   //if no extra ball lose life, reset ball
   else {
-    if(ballX < -1) {
-      ballX = paddleX+1;
-      ballY = paddleY + 4; 
+    if (ballX < -1) {
+      ballX = paddleX + 1;
+      ballY = paddleY + 4;
       dX = 0;
       dY = 0;
       ballLaunched = false;
@@ -191,52 +191,60 @@ void bounceWall() { //(duplicate all conditionals for extraBall, if it is in pla
   }
   //hit a brick
   //parts of this logic taken from ArduBreakout
-  for (int row = 0; row < rows; row ++){
-    for (int column = 0; column < columns; column++){
-      if (!bricks[row][column]){
+  for (int row = 0; row < rows; row ++) {
+    for (int column = 0; column < columns; column++) {
+      if (!bricks[row][column]) {
         //brick bounds
-        int left = columnOffset+5*column;
-        int right = columnOffset+5*column + 4;
+        int left = columnOffset + 5 * column;
+        int right = columnOffset + 5 * column + 4;
         int top = 9 * row;
         int bottom = 9 * row + 8;
 
         //collision
-        if (ballY <= bottom && ballY + 2 >= top && ballX <= right && ballX + 2 >= left){
+        if (ballY <= bottom && ballY + 2 >= top && ballX <= right && ballX + 2 >= left) {
           bricks[row][column] = true;
           hitCount += 1;
-          arduboy.drawRect(columnOffset+5*column, 9*row, 4, 8, 0);
+          arduboy.drawRect(columnOffset + 5 * column, 9 * row, 4, 8, 0);
           //roll to see if an extra ball can be added
-          if (random(0,5) < 2 && extraBall == false) {
+          if (random(0, 6) < 2 && extraBall == false) {
             extraBall = true;
             extraBallX = left;
             extraBallY = top;
             extraDX = 1;
-            extraDY = random(0,2)*2-1;
+            extraDY = random(0, 2) * 2 - 1;
           }
 
           //horizontal bounce
-          if (ballX < left || ballX + 2 > right){
-            dX =- dX;            
+          if (ballX < left || ballX + 2 > right) {
+            dX = - dX;
           }
 
           //vertical bounce
-          if (ballY + 2 > bottom || ballY < top){
-            dY =- dY;
+          if (ballY + 2 > bottom || ballY < top) {
+            dY = - dY;
           }
         }
+      }
+      if (!bricks[row][column]) {
+        //brick bounds
+        int left = columnOffset + 5 * column;
+        int right = columnOffset + 5 * column + 4;
+        int top = 9 * row;
+        int bottom = 9 * row + 8;
+
         //now check for extra ball block collisions
-        if (extraBallY <= bottom && extraBallY + 2 >= top && extraBallX <= right && extraBallX + 2 >= left){
+        if (extraBallY <= bottom && extraBallY + 2 >= top && extraBallX <= right && extraBallX + 2 >= left) {
           bricks[row][column] = true;
           hitCount += 1;
-          arduboy.drawRect(columnOffset+5*column, 9*row, 4, 8, 0);
+          arduboy.drawRect(columnOffset + 5 * column, 9 * row, 4, 8, 0);
           //horizontal bounce
-          if (extraBallX < left || extraBallX + 2 > right){
-            extraDX =- extraDX;            
+          if (extraBallX < left || extraBallX + 2 > right) {
+            extraDX = - extraDX;
           }
 
           //vertical bounce
-          if (extraBallY + 2 > bottom || extraBallY < top){
-            extraDY =- extraDY;
+          if (extraBallY + 2 > bottom || extraBallY < top) {
+            extraDY = - extraDY;
           }
         }
       }
@@ -244,11 +252,11 @@ void bounceWall() { //(duplicate all conditionals for extraBall, if it is in pla
   }
 }
 
-void initialDrawBlocks(){
-  for (int row = 0; row < rows; row ++){
-    for (int column = 0; column < columns; column++){
+void initialDrawBlocks() {
+  for (int row = 0; row < rows; row ++) {
+    for (int column = 0; column < columns; column++) {
       bricks[row][column] = false;
-      arduboy.drawRect(columnOffset+5*column, 9*row, 4, 8, 1);
+      arduboy.drawRect(columnOffset + 5 * column, 9 * row, 4, 8, 1);
     }
   }
 }
